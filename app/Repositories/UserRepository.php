aa82b2dbef5cbd4a2cee8ee7b27b6b63bd56d328
Hoang Nguyen <hoanglc1989@gmail.com>
2020-03-07 16:30:51 +1100

    First Commit

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories\User;
+
+use App\Models\User;
+use App\Repositories\BaseRepository;
+
+class UserRepository extends BaseRepository
+{
+    public function getModel()
+    {
+        return User::class;
+    }
+}
